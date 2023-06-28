054ed0749179848509c75ffec4f1acb1704400d9
mohana <mkamanooru@amplience.com>
2023-06-28 19:50:41 +0100

    Updated INSERT Query

--- sql_python_connection/sql_python_connection.py
+++ sql_python_connection/sql_python_connection.py
@@ -19,7 +19,6 @@ class DbConnectionError:
     pass
 
 
-
 def get_all_records():
     try:
         db_name = 'tests'
@@ -42,6 +41,7 @@ def get_all_records():
         if (db_connection):
             db_connection.close()
             print("Connection Closed Successfully")
+
 
 def get_all_records_with_rep(repName):
     try:
@@ -67,9 +67,52 @@ def get_all_records_with_rep(repName):
             print("Connection Closed Successfully")
 
 
+def insert_new_record(record):
+    try:
+        db_name = 'tests'
+        db_connection = _connect_to_db(db_name)
+        cur = db_connection.cursor()
+        print("Connected to DB: %s" % db_name)
+
+        query = """INSERT INTO abcreport ({}) VALUES ('{}', '{}', '{}', '{}', {}, {}, {})""".format(
+            ', '.join(record.keys()),
+            record['OrderDate'],
+            record['Region'],
+            record['Rep'],
+            record['Item'],
+            record['Units'],
+            record['UnitCost'],
+            record['Total'],
+        )
+        cur.execute(query)
+        db_connection.commit()
+
+        cur.close()
+
+    except Exception:
+        raise DbConnectionError("Failed to read data from DB")
+
+    finally:
+        if (db_connection):
+            db_connection.close()
+            print("Connection Closed Successfully")
+    print("Record added to DB")
+
+record = {
+    'OrderDate': '2020-12-15',
+    'Region': 'Central',
+    'Rep': 'Mohana',
+    'Item': 'post-it-notes',
+    'Units': 220,
+    'UnitCost': 2.5,
+    'Total': 220 * 2.5,
+}
+
 def main():
     # get_all_records()
-    get_all_records_with_rep("Jones")
+    # get_all_records_with_rep("Jones")
+    insert_new_record(record)
+    get_all_records_with_rep("Mohana")
 
 
 if __name__ == '__main__':
